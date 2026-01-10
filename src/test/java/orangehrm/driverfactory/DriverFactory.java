683f0ea4e27b217e5561c38c1daebc5672739a1c
deekshithamgithub <163296974+deekshithamgithub@users.noreply.github.com>
2026-01-10 16:49:50 +0530

    Update DriverFactory.java

--- src/test/java/orangehrm/driverfactory/DriverFactory.java
+++ src/test/java/orangehrm/driverfactory/DriverFactory.java
@@ -8,6 +8,7 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.log4testng.Logger;
@@ -17,7 +18,7 @@ public class DriverFactory {
     // Thread safe driver (parallel execution ke liye)
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    public static void initDriver(String browser) {
+    public static void initDriver(String browser) throws InterruptedException {
     	Reporter.log("login test started",true);
     	Reporter.log(browser,true);
         if (browser == null) {
@@ -35,10 +36,31 @@ public class DriverFactory {
                 break;
 
             case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver.set(new EdgeDriver());
-                break;
+//                WebDriverManager.edgedriver().setup();
+//                System.setProperty("webdriver.edge.driver",
+//                        System.getProperty("user.dir") + "/drivers/msedgedriver.exe");
+//                EdgeOptions options = new EdgeOptions();
+//                options.addArguments("--guest");
+//                options.addArguments("--remote-allow-origins=*");
+//                options.addArguments("--disable-extensions");
+//                options.addArguments("--disable-popup-blocking");
+//                options.addArguments("--inprivate");
+//                options.addArguments("--remote-allow-origins=*");
+//               options.addArguments("start-maximized");
+//                driver.set(new EdgeDriver(options));
+//                break;
+                String edgeDriverPath = System.getProperty("user.dir") + "\\drivers\\msedgedriver.exe";
+                System.setProperty("webdriver.edge.driver", edgeDriverPath);
 
+                EdgeOptions options = new EdgeOptions();
+                options.addArguments("--guest");
+                options.addArguments("--inprivate");
+                options.addArguments("--disable-extensions");
+                options.addArguments("--remote-allow-origins=*");
+
+                driver.set(new EdgeDriver(options));
+                Thread.sleep(2000);   // ✅ Edge stability wait
+                break;
             default:
                 throw new RuntimeException("Invalid browser: " + browser);
         }
@@ -61,10 +83,21 @@ public class DriverFactory {
     }
 
     public static void quitDriver() {
-    	log.info("Quit driver");
-        if (driver.get() != null) {
-            driver.get().quit();
-            driver.remove();
+        try {
+            if (driver.get() != null) {
+                driver.get().quit();
+                driver.remove();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+//        try {
+//            Runtime.getRuntime().exec("taskkill /F /IM msedge.exe");
+//            Runtime.getRuntime().exec("taskkill /F /IM msedgedriver.exe");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
+
 }
