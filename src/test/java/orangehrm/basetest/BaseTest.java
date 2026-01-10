3c3dc7c0769adcceaabbcf9b5f740dd03f760670
deekshithamgithub <163296974+deekshithamgithub@users.noreply.github.com>
2026-01-10 16:53:39 +0530

    Update BaseTest.java

--- src/test/java/orangehrm/basetest/BaseTest.java
+++ src/test/java/orangehrm/basetest/BaseTest.java
@@ -1,6 +1,8 @@
 package orangehrm.basetest;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -15,23 +17,28 @@ public class BaseTest {
 	@BeforeClass
 	public void beforeClass() {
 		try {
+//			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			driver.manage().window().maximize();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	@Parameters("browser")
-	@BeforeMethod
+	@BeforeMethod(alwaysRun = true)
 	public void beforeMethod(String browser) {
-		Reporter.log("Browser is opening",true);
+		Reporter.log("Browser is opening"+browser,true);
 		try {
 			DriverFactory.initDriver(browser);
-			DriverFactory.getDriver()
-            .get(ConfigReader.get("base.url"));
+			driver = DriverFactory.getDriver();
+			driver.get(ConfigReader.get("base.url"));
+			Thread.sleep(5000);
 		}catch(Exception e) {
 			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
-	@AfterMethod
+	@AfterMethod(alwaysRun = true)
 	public void afterMethod() {
 		Reporter.log("Browser is closing",true);
 		try {
