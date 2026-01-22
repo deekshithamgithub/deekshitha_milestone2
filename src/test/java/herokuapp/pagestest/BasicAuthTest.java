cf1c3063a3ec86290ff80de1487b4fd9a3b00ecb
deekshithamgithub <163296974+deekshithamgithub@users.noreply.github.com>
2026-01-22 19:51:22 +0530

    Update BasicAuthTest.java

--- src/test/java/herokuapp/pagestest/BasicAuthTest.java
+++ src/test/java/herokuapp/pagestest/BasicAuthTest.java
@@ -9,10 +9,14 @@ public class BasicAuthTest extends BaseTest {
 
     @Test
     public void tc01_verifyBasicAuth() {
+        try{
         // Basic Auth requires credentials in URL
         driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
 
         BasicAuthPage auth = new BasicAuthPage(driver);
         Assert.assertTrue(auth.getMessageText().contains("Congratulations"));
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
