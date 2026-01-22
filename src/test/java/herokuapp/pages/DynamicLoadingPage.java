995544d155c98126fbd4762ae1732688e39e0f0d
deekshithamgithub <163296974+deekshithamgithub@users.noreply.github.com>
2026-01-22 19:35:48 +0530

    Update DynamicLoadingPage.java

--- src/test/java/herokuapp/pages/DynamicLoadingPage.java
+++ src/test/java/herokuapp/pages/DynamicLoadingPage.java
@@ -14,17 +14,25 @@ public class DynamicLoadingPage {
     private WebDriver driver;
 
     public DynamicLoadingPage(WebDriver driver) {
+        try{
         Reporter.log("Initializing DynamicLoadingPage", true);
         this.driver = driver;
         PageFactory.initElements(driver, this);
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FindBy(linkText = "Example 2: Element rendered after the fact")
     private WebElement example2;
 
     public void openExample2() {
+         try{
         Reporter.log("Opening Dynamic Loading Example 2", true);
         example2.click();
         log.info("Example 2 opened");
+             } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
