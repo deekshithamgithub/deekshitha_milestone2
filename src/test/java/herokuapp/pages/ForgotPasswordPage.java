8332270313d67ff7e231aafc9ba2798373700e1c
deekshithamgithub <163296974+deekshithamgithub@users.noreply.github.com>
2026-01-22 19:39:55 +0530

    Update ForgotPasswordPage.java

--- src/test/java/herokuapp/pages/ForgotPasswordPage.java
+++ src/test/java/herokuapp/pages/ForgotPasswordPage.java
@@ -15,9 +15,13 @@ public class ForgotPasswordPage {
     private WebDriver driver;
 
     public ForgotPasswordPage(WebDriver driver) {
+        try{
         Reporter.log("Initializing ForgotPasswordPage", true);
         this.driver = driver;
         PageFactory.initElements(driver, this);
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FindBy(id = "email")
@@ -27,17 +31,25 @@ public class ForgotPasswordPage {
     private WebElement retrieveBtn;
 
     public void enterEmail(String mail) {
+        try{
         Reporter.log("Entering email: " + mail, true);
         WaitUtil.waitForVisibility(driver, email);
         email.clear();
         email.sendKeys(mail);
         log.info("Email entered");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void clickRetrievePassword() {
+        try{
         Reporter.log("Clicking retrieve password button", true);
         WaitUtil.waitForClick(driver, retrieveBtn);
         retrieveBtn.click();
         log.info("Retrieve clicked");
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
