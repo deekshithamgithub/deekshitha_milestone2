3fa82866385833731e4af688100c1912c9f130a0
deekshithamgithub <163296974+deekshithamgithub@users.noreply.github.com>
2026-01-22 19:49:58 +0530

    Update NotificationMessagesPage.java

--- src/test/java/herokuapp/pages/NotificationMessagesPage.java
+++ src/test/java/herokuapp/pages/NotificationMessagesPage.java
@@ -16,9 +16,13 @@ public class NotificationMessagesPage {
     private WebDriver driver;
 
     public NotificationMessagesPage(WebDriver driver) {
+        try{
         Reporter.log("Initializing NotificationMessagesPage", true);
         this.driver = driver;
         PageFactory.initElements(driver, this);
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FindBy(linkText = "Click here")
@@ -28,14 +32,22 @@ public class NotificationMessagesPage {
     private WebElement flashMsg;
 
     public void clickNotification() {
+        try{
         Reporter.log("Clicking notification", true);
         WaitUtil.waitForClick(driver, clickHere);
         clickHere.click();
         log.info("Notification clicked");
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String getMessage() {
+        try{
         WaitUtil.waitForVisibility(driver, flashMsg);
         return flashMsg.getText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
